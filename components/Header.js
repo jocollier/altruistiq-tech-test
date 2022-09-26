d5238f720492c5aed83206de713fcae6e0434d11
jocollier <jocollier.it@gmail.com>
2022-09-26 07:43:58 +0100

    Created Header for app, Added getstaticprops to home & deleted style sheet

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Header.module.scss'
+
+const Header = () => {
+    return (
+        <div className={styles.title}>
+            <h1 className={styles.headerTitle}>Historical global carbon footprint</h1>
+        </div>
+    )
+}
+
+export default Header
